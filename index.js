8c0d79874483e8b41c896a351fabc8bdeeea225e
Ajit Yadav <yadavajit8916@gmail.com>
2023-01-08 22:56:24 +0530

    m

--- index.js
+++ index.js
@@ -7,10 +7,14 @@ const app = express()
 const PORT =  process.env.PORT | 5000
 
 
-// const readStream = fs.createReadStream('exams.zip')
+const readStream = fs.createReadStream('exams.zip')
 app.get('/',(req,res)=>{
     // res.setHeader('Content-Disposition', 'attachment; filename="exams.pdf"').download('exams.zip')
-    send(req,'./exams.zip').pipe(res)
+    // send(req,'./exams.zip').pipe(res)
+    res.set('Content-Type','application/zip');
+    res.set('Content-Disposition',`attachment; filename=download.zip`);
+    res.download('exams.zip')
+
 })
 
 
