20bcf7c29bfc958190e54e2cfde35c8701dd3672
Ajit Yadav <yadavajit8916@gmail.com>
2023-01-08 22:26:32 +0530

    minor change

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const express = require("express")
+const fs = require("fs")
+const { Stream } = require("stream")
 
 const app = express()
 
 const PORT =  process.env.PORT | 5000
 
 
-
+// const readStream = fs.createReadStream('exams.zip')
 app.get('/',(req,res)=>{
-
-    res.download('./exams.zip')
+    res.setHeader('Content-Disposition', 'attachment; filename="exams.pdf"').download('exams.zip')
 })
 
 
